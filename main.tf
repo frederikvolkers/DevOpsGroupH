de1515b1a00ebcfbc2d1d62d4fe4e09937953752
frvo <frvo@itu.dk>
2021-04-29 13:59:21 +0200

    aks ouput updated
    Signed-off-by: frvo <frvo@itu.dk>

--- main.tf
+++ main.tf
@@ -253,12 +253,28 @@ output "app_service_default_hostname" {
   value = "https://${azurerm_app_service.app-service.default_site_hostname}"
 }
 
-output "client_certificate" {
-    value = azurerm_kubernetes_cluster.aks.kube_config.0.client_certificate
+output "id" {
+  value = azurerm_kubernetes_cluster.example.id
 }
 
 output "kube_config" {
-    value = azurerm_kubernetes_cluster.aks.kube_config_raw
+  value = azurerm_kubernetes_cluster.example.kube_config_raw
+}
+
+output "client_key" {
+  value = azurerm_kubernetes_cluster.example.kube_config.0.client_key
+}
+
+output "client_certificate" {
+  value = azurerm_kubernetes_cluster.example.kube_config.0.client_certificate
+}
+
+output "cluster_ca_certificate" {
+  value = azurerm_kubernetes_cluster.example.kube_config.0.cluster_ca_certificate
+}
+
+output "host" {
+  value = azurerm_kubernetes_cluster.example.kube_config.0.host
 }
 
 
