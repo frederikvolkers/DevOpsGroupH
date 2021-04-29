5493ebcff5e14a4b41e09cf8b668dbc8a9ca997f
frvo <frvo@itu.dk>
2021-04-29 14:23:51 +0200

    removed kube config
    Signed-off-by: frvo <frvo@itu.dk>

--- main.tf
+++ main.tf
@@ -257,10 +257,6 @@ output "id" {
   value = azurerm_kubernetes_cluster.aks.id
 }
 
-output "kube_config" {
-  value = azurerm_kubernetes_cluster.aks.kube_config_raw
-}
-
 output "client_key" {
   value = azurerm_kubernetes_cluster.aks.kube_config.0.client_key
 }
